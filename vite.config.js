22fe63086036cdec9b0c8c9bb42cb56d05801a12
vinayyellaram <vinayyellaram715@gmail.com>
2025-12-07 17:00:46 +0530

    feat: add gh-pages deployment script and update package dependencies refactor: streamline vite configuration by removing unnecessary server settings

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: 5000,
-  },
+  base: "/Portfolio",
 });
