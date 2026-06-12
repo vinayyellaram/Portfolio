43da3712abeae5822d67f6f7ae367750fedb4aa8
root <root@srv1348635.hstgr.cloud>
2026-06-12 17:23:11 +0000

    commented logbook , not to show on f.e

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
         <Projects />
         <Skills />
         <Services />
-        <Logbook />
+        {/* <Logbook /> */}
         <Contact />
       </main>
       <Footer />
